ffea35ed22740aaa19a07d21e04c9128ec2416d7
Khaled-abousheikh <Khaled-abousheikh@gmail.com>
2020-07-12 15:05:28 +0300

    db working

---
+++ Back-end/server/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const database = require("../db/index");
+let expensesModel = database.expensesModel;
+let app = express();
+
+app.use(express.json());
+// app.use(express.static(__dirname + '/../front-end/dist'));
+app.use(express.static("dist"))
+
+    app.post("/expenses",(req,res)=>{
+	const {text ,amount,createdAt}= req.body;
+    let expensesDocument = new expensesModel({text: text , amount:amount , createdAt:createdAt});
+
+    expensesDocument.save((err)=>{
+     if(err){
+        console.log(err);
+        res.status(500).send(err);
+     }else{
+        res.status(201).send("Saved expenses !");
+     }
+});
+});
+
+
+app.get("/expenses",(req,res) =>{
+expensesModel.find({})
+   .then((result) =>{
+	res.send(result);
+}).catch(err =>{
+	res.send(err);
+});
+});
+
+//search by spec :
+app.get("/expenses/:spec",(req , res) =>{
+    const specVal = req.params.spec;
+    expensesModel.find({spec: specVal}).then(result =>{
+    	res.send(result);
+    }).catch(err =>{
+    	res.send(err);
+    });
+});
+
+
+var port = 4000; //default port
+app.listen(port, ()=>{
+	console.log(`app listen to port ${port}`);
+});
+
+
