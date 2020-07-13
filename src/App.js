eba1b138e1b62dc3b4533e288a017994712ba819
Khaled-abousheikh <Khaled-abousheikh@gmail.com>
2020-07-13 11:34:25 +0300

    db and server using react

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AddTransaction from "./Components/AddTransaction/AddTransaction"
 function App() {
   return (
     <div className="App">
-     <h1> Test </h1>
+     <h1> Abed </h1>
       <AddTransaction/>
       
     </div>
