4b1e0bbd1af8519556aa7f6145ae373e0e763fb3
Khaled-abousheikh <Khaled-abousheikh@gmail.com>
2020-07-16 09:49:54 +0300

    Updates

---
+++ precache-manifest.be62a7d311f248bc4bd26eb81e215c0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e670e66b2cb8c514e190f36a7f5966d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56a86e8297d457a78822",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "550f01aa7543abda8a71",
+    "url": "/static/js/2.bc3354bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc3354bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a86e8297d457a78822",
+    "url": "/static/js/main.ad95b140.chunk.js"
+  },
+  {
+    "revision": "66713393aa82ab66da1a",
+    "url": "/static/js/runtime-main.487450d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
